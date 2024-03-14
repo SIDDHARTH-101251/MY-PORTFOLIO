af8d7617c925f70de6ebd6940f49b4070bf94a03
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-03-14 23:15:32 +0530

    Reinitialized a git repo

---
+++ portfolio.js
@@ -0,0 +1,47 @@
+let flag = 0;
+
+function projectsStyle() {
+    let dis = document.getElementById("paraButton");
+    if (flag % 2 === 0) {
+        dis.style.display = "block";
+    } else {
+        dis.style.display = "none";
+    }
+    flag = flag + 1;
+}
+
+let flag1 = 0;
+
+function projectsStyle1() {
+    let dis = document.getElementById("paraButton1");
+    if (flag1 % 2 === 0) {
+        dis.style.display = "block";
+    } else {
+        dis.style.display = "none";
+    }
+    flag1 = flag1 + 1;
+}
+
+let flag2 = 0;
+
+function projectsStyle2() {
+    let dis = document.getElementById("paraButton2");
+    if (flag2 % 2 === 0) {
+        dis.style.display = "block";
+    } else {
+        dis.style.display = "none";
+    }
+    flag2 = flag2 + 1;
+}
+
+let flag3 = 0;
+
+function projectsStyle3() {
+    let dis = document.getElementById("paraButton3");
+    if (flag3 % 2 === 0) {
+        dis.style.display = "block";
+    } else {
+        dis.style.display = "none";
+    }
+    flag3 = flag3 + 1;
+}
